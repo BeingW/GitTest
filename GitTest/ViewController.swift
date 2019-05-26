14fc4f4a60a7604267e6f120a2d9a89f3f5b906e
BeingW <abraxas.jk@gmail.com>
2019-05-26 13:30:26 +0900

    first commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Jae Ki Lee on 5/26/19.
+//  Copyright © 2019 Jae Ki Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
