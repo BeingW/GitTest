c490ecfcbdfea65f6b4142dfde365fac61a0c86f
BeingW <abraxas.jk@gmail.com>
2019-05-26 14:01:53 +0900

    Rechange B

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     let changeA = "changeA"
     
+    let changeB = "changeB"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
